7b756e03936afd43dd363952067a727ed22af2ea
zotzu2000 <63327702+zotzu2000@users.noreply.github.com>
2020-04-10 08:18:14 +0800

    Enemy

--- ClassField/Assets/Enemy.cs
+++ ClassField/Assets/Enemy.cs
@@ -1,18 +1,64 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NewBehaviourScript : MonoBehaviour
+﻿using UnityEngine;
+/// <summary>
+/// 定義怪物物件
+/// </summary>
+public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /// <summary>
+    /// 怪物的血量
+    /// </summary>
+    [Header("血量"), Range(50, 500)]
+    [Tooltip("怪物的血量")]
+    public int hp ;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    /// <summary>
+    /// 怪物的魔力
+    /// </summary>
+    [Header("魔力"), Range(10, 100)]
+    [Tooltip("怪物的魔力")]
+    public int mp = 100;
+
+    /// <summary>
+    /// 移動速度
+    /// </summary>
+    [Header("能力值")]
+    [Range(1,50.5f)]
+    public float speed = 10.5f;
+
+    /// <summary>
+    /// 攻擊力
+    /// </summary>
+    [Header("攻擊力"), Range(20, 200)]
+    public int atk = 50;
+
+    /// <summary>
+    /// 防禦力
+    /// </summary>
+    [Header("防禦力"), Range(0, 100)]
+    public int mtd ;
+
+    /// <summary>
+    /// 武器名稱
+    /// </summary>
+    [Header("裝備")]
+    public string hh ;
+
+    /// <summary>
+    /// 裝備名稱
+    /// </summary>
+    public string bb = "皮衣";
+
+    /// <summary>
+    /// 是否帶有鑰匙
+    /// </summary>
+    [Header("其他資料")]
+    [Tooltip("怪物是否帶有鑰匙")]
+    public bool key = false;
+
+    /// <summary>
+    /// 是否掉落寶物
+    /// </summary>
+    [Tooltip("怪物是否帶有寶物")]
+    public bool box = false;
 }
+
--- ClassField/Assets/EnemyManager.cs
+++ ClassField/Assets/EnemyManager.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    
+    // 定義類別成員
+    // c#
+    // 1.大小不一樣
+    // 2. 括號成對出現 () [] {} '' "" <>
 
-    // Update is called once per frame
-    void Update()
+    [Header("怪物一號")]
+    public Enemy Enemy1;
+
+    [Header("怪物二號")]
+    public Enemy Enemy2;
+
+    // 事件 : 在特定時間會已指定次數執行的方法 (名稱為藍色)
+    // 開始事件 : 撥放遊戲時執行一次 - 初始事件
+    private void Start()
     {
-        
+        // 輸出("文字訊息");
+        // c# 結尾為分號 ;
+
+        // 取得 get
+        print("輸出怪物一號的魔力 : " + Enemy1.mp);
+        print("輸出怪物二號的裝備名稱 : " + Enemy2.bb);
+
+        // 設定 set
+        Enemy1.hp = 100;
+        Enemy2.hp = 300;
+        Enemy1.mtd = 50;
+        Enemy1.hh ="木棍";
+        Enemy2.hh = "短刀";
+        Enemy1.key = true;
+        Enemy2.box = true;
     }
 }
+
